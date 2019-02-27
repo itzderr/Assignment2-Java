64ff48a4eeea4e7e93b18fe506fb145cffe36128
FilipeMalachias <Filipe_malachias@hotmail.com>
2019-02-27 11:48:50 -0800

    [Try to use git to send final assignment2-Java]

--- src/main/java/ca/ciccc/Assignment2.java
+++ src/main/java/ca/ciccc/Assignment2.java
@@ -1,5 +1,7 @@
 package ca.ciccc;
 
+import java.util.Scanner;
+
 /**
  * This is the starter file for the Assignment 2
  *
@@ -23,8 +25,26 @@ public class Assignment2 {
      */
     public String numberLoops(int n) {
         // Todo: your code goes here
+        String numbers = "";
+        for (int i = 1; i <= n; ++i) {
+            String number = "";
+            for (int j = 0; j < n - i; ++j)
+            {
+                number += ".";
+            }
+            for (int j = 0; j < i; ++j)
+            {
+                number += String.format("%d", i);
+            }
+            numbers += number;
 
-        return null;
+            if (i < n)
+            {
+                numbers += "\n";
+            }
+        }
+
+        return numbers;
     }
 
     /**
@@ -34,9 +54,19 @@ public class Assignment2 {
      */
     public boolean countNumbers(int[] nums) {
         // Todo: your code goes here
-
-        return false;
+        int numberOne = 0;
+        int numberSeven = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] == 1) {
+                numberOne += 1;
+            }
+            if (nums[i] == 7) {
+                numberSeven += 1;
+            }
+        }
+        return numberOne > numberSeven;
     }
+
 
     /**
      * Return the sum of numbers in the array, returning 0 for an empty array. Except the number 13
@@ -52,8 +82,17 @@ public class Assignment2 {
      */
     public int sumExcept13(int[] nums) {
         // Todo: your code goes here
-
-        return 0;
+        int tot = 0;
+        for (int i = 0; i < nums.length; ++i)
+        {
+            if (nums[i] == 13)
+            {
+                ++i;
+                continue;
+            }
+            tot += nums[i];
+        }
+        return tot;
     }
 
     /**
@@ -69,8 +108,20 @@ public class Assignment2 {
      */
     public int[] shiftArray(int[] nums) {
         // Todo: your code goes here
+        int[] shift = new int[nums.length];
+        for (int i = 0; i < nums.length; ++i)
+        {
+            if (i == 0)
+            {
+                shift[shift.length - 1] = nums[i];
+            }
+            else
+            {
+                shift[i - 1] = nums[i];
+            }
 
-        return null;
+        }
+        return shift;
     }
 
     /**
@@ -80,6 +131,24 @@ public class Assignment2 {
      */
     public boolean tripleIncreasingOrder(int[] nums) {
         // Todo: your code goes here
+        int increasing = 1;
+        int lastNumber = nums[0];
+        for (int i = 1; i < nums.length; ++i)
+        {
+            if (nums[i] == lastNumber + 1)
+            {
+                increasing += 1;
+            }
+            else
+            {
+                increasing = 1;
+            }
+            lastNumber = nums[i];
+            if (increasing == 3)
+            {
+                return true;
+            }
+        }
 
         return false;
     }
@@ -91,8 +160,20 @@ public class Assignment2 {
      */
     public boolean evenOrOdd(int[] nums){
         // Todo: your code goes here
-
-        return false;
+        int even = 0;
+        int odd = 0;
+        for (int i = 0; i < nums.length; ++i)
+        {
+            if (nums[i] % 2 == 0)
+            {
+                even += 1;
+            }
+            else
+                {
+                    odd += 1;
+                }
+        }
+        return even == odd;
     }
 }
 
