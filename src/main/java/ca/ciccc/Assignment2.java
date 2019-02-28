a68043534c6c2f3e594ac39a20268ec7ba885a89
Sea <bnly22335@gmail.com>
2019-02-28 11:27:07 -0800

    Finish

--- src/main/java/ca/ciccc/Assignment2.java
+++ src/main/java/ca/ciccc/Assignment2.java
@@ -11,50 +11,37 @@ import java.util.Scanner;
 public class Assignment2 {
     /**
      * Write nested for-loops to produce the following output from the given input n.
+     *
      * @param n number to create a pattern.
      * @return pattern
-     *
+     * <p>
      * Sample Input / Output
      * numberLoops(3);
-     *
+     * <p>
      * Sample Output
      * ..1
      * .22
      * 333
-     *
      */
-    public String numberLoops(int n) {
-        String numbers = "";
-        for (int i = 1; i <= n; ++i) {
-            String number = "";
-            for (int j = 0; j < n - i; ++j)
-            {
-                number += ".";
-            }
-            for (int j = 0; j < i; ++j)
-            {
-                number += String.format("%d", i);
-            }
-            numbers += number;
-
-            if (i < n)
-            {
-                numbers += "\n";
-            }
+    public String numberLoops(int n){
+        String result = "";
+        for (int i = 1; i <= n; i++) {
+            for (int k = n - i; k >= 1; k--)
+                result += "*";
+            for (int j = 1; j <= i; j++)
+                result += i;
         }
-
-        return numbers;
+        return result;
     }
-
 
 
     /**
      * Given an array of integers, return true if the number of 1s is greater than the number of 7s
+     *
      * @param nums array of ints
      * @return true if the number of 1s is greater than the number of 7s
      */
     public boolean countNumbers(int[] nums) {
-        // Todo: your code goes here
         int numberOne = 0;
         int numberSeven = 0;
         for (int i = 0; i < nums.length; ++i) {
@@ -73,20 +60,20 @@ public class Assignment2 {
      * Return the sum of numbers in the array, returning 0 for an empty array. Except the number 13
      * is very unlucky, so it does not count the number that comes immediately after a 13, also 13 itself
      * does not count.
+     *
      * @param nums array of numbers
      * @return the sum except for 13
-     *
+     * <p>
      * Sample Input / Output
      * sumExcept13({1, 2, 2, 1}) -> 6
      * sumExcept13({1, 2, 2, 1, 13} -> 6
      * sumExcept13({1, 2, 13, 2, 1, 13} -> 4
      */
     public int sumExcept13(int[] nums) {
+
         int tot = 0;
-        for (int i = 0; i < nums.length; ++i)
-        {
-            if (nums[i] == 13)
-            {
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] == 13) {
                 ++i;
                 continue;
             }
@@ -95,12 +82,14 @@ public class Assignment2 {
         return tot;
     }
 
+
     /**
      * Return an array that is "left shifted" by one -- so {6, 2, 5, 3} return {2, 5, 3, 6}.
      * You may modify and return the given array, or return a new array.
+     *
      * @param nums array of ints
      * @return shifted array
-     *
+     * <p>
      * Sample Input / Output
      * shiftArray({6, 2, 5, 3}) -> {2, 4, 3, 6}
      * shiftArray({2, 3}) -> {3, 2}
@@ -108,14 +97,10 @@ public class Assignment2 {
      */
     public int[] shiftArray(int[] nums) {
         int[] shift = new int[nums.length];
-        for (int i = 0; i < nums.length; ++i)
-        {
-            if (i == 0)
-            {
+        for (int i = 0; i < nums.length; ++i) {
+            if (i == 0) {
                 shift[shift.length - 1] = nums[i];
-            }
-            else
-            {
+            } else {
                 shift[i - 1] = nums[i];
             }
 
@@ -123,31 +108,27 @@ public class Assignment2 {
         return shift;
     }
 
+
     /**
      * Return true if the array contains three increasing adjacent numbers, otherwise false.
+     *
      * @param nums array of ints.
      * @return true if the array contains three increasing adjacent numbers, otherwise false.
      */
     public boolean tripleIncreasingOrder(int[] nums) {
         int increasing = 1;
         int lastNumber = nums[0];
-        for (int i = 1; i < nums.length; ++i)
-        {
-            if (nums[i] == lastNumber + 1)
-            {
-                increasing += 1;
-            }
-            else
-            {
+        for (int i = 1; i < nums.length; ++i) {
+            if (nums[i] == lastNumber + 1) {
+                increasing = increasing + 1;
+            } else {
                 increasing = 1;
             }
             lastNumber = nums[i];
-            if (increasing == 3)
-            {
+            if (increasing == 3) {
                 return true;
             }
         }
-
         return false;
     }
 
@@ -159,18 +140,16 @@ public class Assignment2 {
     public boolean evenOrOdd(int[] nums){
         int even = 0;
         int odd = 0;
-        for (int i = 0; i < nums.length; ++i)
-        {
-            if (nums[i] % 2 == 0)
-            {
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] % 2 == 0) {
                 even += 1;
             }
-            else
-            {
+            else {
                 odd += 1;
             }
         }
         return even == odd;
     }
+
 }
 
