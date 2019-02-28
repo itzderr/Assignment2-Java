43b76f7764ceb8aa3a7186cbb03b1de5fe5c09c0
emremungan <47868129+emremungan@users.noreply.github.com>
2019-02-28 09:24:28 -0800

    Add files via upload

---
+++ MagicSquare.java
@@ -0,0 +1,117 @@
+package ca.ciccc;
+
+/**
+ *
+ * Magic Squares are square arrays of numbers that have the interesting property that
+ * the numbers in each column, and in each row, all add up to the same total.
+ *
+ * Given an n x n square of numbers, determine if it is magic square.
+ *
+ * Reference: {@link java.lang.Integer}
+ * Integer is a wrapper class for {@code int} primitive type.
+ */
+public class MagicSquare {
+    /* n rows and n cols square */
+    private int n;
+    /* 2-Dimensional array to represent the square */
+    private Integer[][] square;
+
+    /**
+     * Constructor that takes n as the number of rows and cols.
+     *
+     * @param n number of rows and cols.
+     */
+    public MagicSquare(int n) {
+        this.n = n;
+    }
+
+    /**
+     * Constructor that takes n as the number of rows and cols.
+     * Initializes square 2-Dimensional array.
+     *
+     * @param n      number of rows and cols.
+     * @param square
+     */
+    public MagicSquare(int n, Integer[][] square) {
+        this.n = n;
+        this.square = square;
+    }
+
+    /**
+     * Getter for square
+     *
+     * @return {@code this.square}
+     */
+    public Integer[][] getSquare() {
+        return square;
+    }
+
+    /**
+     * Setter for square.
+     *
+     * @param square
+     */
+    public void setSquare(Integer[][] square) {
+        this.square = square;
+    }
+
+    /**
+     * Check if {@code square} is magic square or not.
+     *
+     * @param square 2-Dimensional array
+     * @return {@code true} if {@code square} is magic, otherwise {@code false}
+     * <p>
+     * Sample Input
+     * {
+     * {16, 3, 2, 13},
+     * {5, 10, 11, 8},
+     * {9, 6, 7, 12},
+     * {4, 15, 14, 1}
+     * }
+     * <p>
+     * Sample Output
+     * true
+     * <p>
+     * Explanation: The sums of each row and col are equal.
+     */
+    public static boolean isMagicSquare(Integer[][] square) {
+        int total = 0;
+        int sqH = square.length;
+        for (int i = 0; i < sqH; ++i) {
+            int firstRow = 0;
+            for (int j = 0; j < sqH; ++j) {
+                firstRow += square[j][i];
+            }
+            total = firstRow;
+        }
+        for (int j = 1; j < sqH; ++j) {
+            int rowTotal = 0;
+            for (int i = 0; i < sqH; ++i) {
+                rowTotal += square[j][i];
+            }
+            if (rowTotal != total) {
+                return false;
+            }
+        }
+        for (int i = 0; i < sqH; ++i) {
+            int col = 0;
+            for (int j = 0; j < sqH; ++j) {
+                col += square[j][i];
+            }
+            if (col != total) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+
+    /**
+     * Check if {@code this.square} is magic square or not.
+     * @return {@code true} if {@code square} is magic, otherwise {@code false}
+     */
+    public boolean isMagicSquare() {
+        return isMagicSquare(this.square);
+    }
+}
