45f55555158c8906bbd9a120b549ff0194f81375
Cayo <cayomesquita@gmail.com>
2020-02-19 11:05:15 -0800

    Resolve Assignment2 and MagicSquare

--- src/main/java/ca/ciccc/Assignment2.java
+++ src/main/java/ca/ciccc/Assignment2.java
@@ -9,90 +9,128 @@ package ca.ciccc;
 public class Assignment2 {
     /**
      * Write nested for-loops to produce the following output from the given input n.
+     *
      * @param n number to create a pattern.
      * @return pattern
-     *
+     * <p>
      * Sample Input / Output
      * numberLoops(3);
-     *
+     * <p>
      * Sample Output
      * ..1
      * .22
      * 333
-     *
      */
     public String numberLoops(int n) {
-        // Todo: your code goes here
-
-        return null;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                sb.append(j <= (n - i) ? "." : i);
+            }
+            sb.append(i == n ? "" : "\n");
+        }
+        return sb.toString();
     }
 
     /**
      * Given an array of integers, return true if the number of 1s is greater than the number of 7s
+     *
      * @param nums array of ints
      * @return true if the number of 1s is greater than the number of 7s
      */
     public boolean countNumbers(int[] nums) {
-        // Todo: your code goes here
+        int sum1 = 0;
+        int sum7 = 0;
+        for (int num : nums) {
+            if (num == 1) {
+                sum1++;
+            } else if (num == 7) {
+                sum7++;
+            }
 
-        return false;
+        }
+        return sum1 > sum7;
     }
 
     /**
      * Return the sum of numbers in the array, returning 0 for an empty array. Except the number 13
      * is very unlucky, so it does not count the number that comes immediately after a 13, also 13 itself
      * does not count.
+     *
      * @param nums array of numbers
      * @return the sum except for 13
-     *
+     * <p>
      * Sample Input / Output
      * sumExcept13({1, 2, 2, 1}) -> 6
      * sumExcept13({1, 2, 2, 1, 13} -> 6
      * sumExcept13({1, 2, 13, 2, 1, 13} -> 4
      */
     public int sumExcept13(int[] nums) {
-        // Todo: your code goes here
-
-        return 0;
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 13) {
+                i++;
+            } else {
+                sum += nums[i];
+            }
+        }
+        return sum;
     }
 
     /**
      * Return an array that is "left shifted" by one -- so {6, 2, 5, 3} return {2, 5, 3, 6}.
      * You may modify and return the given array, or return a new array.
+     *
      * @param nums array of ints
      * @return shifted array
-     *
+     * <p>
      * Sample Input / Output
      * shiftArray({6, 2, 5, 3}) -> {2, 4, 3, 6}
      * shiftArray({2, 3}) -> {3, 2}
      * shiftArray({7}) -> {7}
      */
     public int[] shiftArray(int[] nums) {
-        // Todo: your code goes here
-
-        return null;
+        if (nums == null || nums.length < 2) {
+            return nums;
+        }
+        int aux = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            nums[i - 1] = nums[i];
+        }
+        nums[nums.length - 1] = aux;
+        return nums;
     }
 
     /**
      * Return true if the array contains three increasing adjacent numbers, otherwise false.
+     *
      * @param nums array of ints.
      * @return true if the array contains three increasing adjacent numbers, otherwise false.
      */
     public boolean tripleIncreasingOrder(int[] nums) {
-        // Todo: your code goes here
-
+        if (nums == null || nums.length < 3) {
+            return false;
+        }
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (nums[i + 1] == nums[i] + 1 && nums[i + 2] == nums[i] + 2) {
+                return true;
+            }
+        }
         return false;
     }
 
     /**
      * Return true if the array contains the same number of odds and even numbers, otherwise false.
+     *
      * @param nums array of ints.
      * @return true if the array contains the same number of odds and even numbers, otherwise false.
      */
-    public boolean evenOrOdd(int[] nums){
-        // Todo: your code goes here
-
-        return false;
+    public boolean evenOrOdd(int[] nums) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += (num % 2 == 0) ? 1 : -1;
+        }
+        return sum == 0;
     }
 }
 
