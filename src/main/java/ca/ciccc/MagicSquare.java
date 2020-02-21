27107646fe8da0a0c2124d073971e01a4a47b9f2
version-1 <j.onodera26@gmail.com>
2020-02-20 18:11:44 -0800

    implement magic square

--- src/main/java/ca/ciccc/MagicSquare.java
+++ src/main/java/ca/ciccc/MagicSquare.java
@@ -1,14 +1,17 @@
 package ca.ciccc;
 
+import java.util.Arrays;
+
 /**
  *
- * Magic Squares are square arrays of numbers that have the interesting property that
- * the numbers in each column, and in each row, all add up to the same total.
+ * Magic Squares are square arrays of numbers that have the interesting property
+ * that the numbers in each column, and in each row, all add up to the same
+ * total.
  *
  * Given an n x n square of numbers, determine if it is magic square.
  *
- * Reference: {@link java.lang.Integer}
- * Integer is a wrapper class for {@code int} primitive type.
+ * Reference: {@link java.lang.Integer} Integer is a wrapper class for
+ * {@code int} primitive type.
  */
 public class MagicSquare {
     /* n rows and n cols square */
@@ -18,6 +21,7 @@ public class MagicSquare {
 
     /**
      * Constructor that takes n as the number of rows and cols.
+     *
      * @param n number of rows and cols.
      */
     public MagicSquare(int n) {
@@ -25,9 +29,10 @@ public class MagicSquare {
     }
 
     /**
-     * Constructor that takes n as the number of rows and cols.
-     * Initializes square 2-Dimensional array.
-     * @param n number of rows and cols.
+     * Constructor that takes n as the number of rows and cols. Initializes square
+     * 2-Dimensional array.
+     *
+     * @param n      number of rows and cols.
      * @param square
      */
     public MagicSquare(int n, Integer[][] square) {
@@ -37,6 +42,7 @@ public class MagicSquare {
 
     /**
      * Getter for square
+     *
      * @return {@code this.square}
      */
     public Integer[][] getSquare() {
@@ -45,6 +51,7 @@ public class MagicSquare {
 
     /**
      * Setter for square.
+     *
      * @param square
      */
     public void setSquare(Integer[][] square) {
@@ -53,29 +60,64 @@ public class MagicSquare {
 
     /**
      * Check if {@code square} is magic square or not.
+     *
      * @param square 2-Dimensional array
      * @return {@code true} if {@code square} is magic, otherwise {@code false}
      *
-     * Sample Input
-     * {
-     *   {16, 3, 2, 13},
-     *   {5, 10, 11, 8},
-     *   {9, 6, 7, 12},
-     *   {4, 15, 14, 1}
-     * }
+     *         Sample Input { {16, 3, 2, 13}, {5, 10, 11, 8}, {9, 6, 7, 12}, {4, 15,
+     *         14, 1} }
      *
-     * Sample Output
-     * true
+     *         Sample Output true
      *
-     * Explanation: The sums of each row and col are equal.
+     *         Explanation: The sums of each row and col are equal.
      */
     public static boolean isMagicSquare(Integer[][] square) {
-        // TODO: You need to implement this method.
-        return false;
+        boolean flg = true;
+        int sum = 0;
+        int[] colSum = new int[square.length];
+        for (int i = 0; i < square.length; i++) {
+            Integer[] row = square[i];
+            int rowSum = 0;
+            for (int j = 0; j < row.length; j++) {
+                int num = row[j];
+                if (i == 0) {
+                    sum += num;
+                } else {
+                    rowSum += num;
+                }
+                colSum[j] += num;
+            }
+            if (i > 0 && rowSum != sum) {
+                flg = false;
+                break;
+            }
+        }
+        if (!flg) {
+            return false;
+        }
+        return hasEqualSums(colSum);
+    }
+
+    private static boolean hasEqualSums(int[] nums) {
+        if (nums.length <= 1) {
+            return true;
+        }
+        boolean flg = true;
+        for (int i = 0; i < nums.length; i++) {
+            if (i < 1) {
+                continue;
+            }
+            if (nums[i - 1] != nums[i]) {
+                flg = false;
+                break;
+            }
+        }
+        return flg;
     }
 
     /**
      * Check if {@code this.square} is magic square or not.
+     *
      * @return {@code true} if {@code square} is magic, otherwise {@code false}
      */
     public boolean isMagicSquare() {
