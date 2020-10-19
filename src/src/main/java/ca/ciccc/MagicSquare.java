ebda9795040e435ca053f1c14557cdedaa03c558
rlarlejr88 <72488566+rlarlejr88@users.noreply.github.com>
2020-10-19 15:37:53 -0700

    Add files via upload

---
+++ src/src/main/java/ca/ciccc/MagicSquare.java
@@ -0,0 +1,102 @@
+package ca.ciccc;
+
+/**
+ * Magic Squares are square arrays of numbers that have the interesting property that the numbers in
+ * each column, and in each row, all add up to the same total.
+ * <p>
+ * Given an n x n square of numbers, determine if it is magic square.
+ * <p>
+ * Reference: {@link java.lang.Integer} Integer is a wrapper class for {@code int} primitive type.
+ */
+public class MagicSquare {
+
+  /* n rows and n cols square */
+  private int n;
+  /* 2-Dimensional array to represent the square */
+  private Integer[][] square;
+
+  /**
+   * Constructor that takes n as the number of rows and cols.
+   *
+   * @param n number of rows and cols.
+   */
+  public MagicSquare(int n) {
+    this.n = n;
+    this.square = new Integer[n][n];
+  }
+
+  /**
+   * Constructor that takes n as the number of rows and cols. Initializes square 2-Dimensional
+   * array.
+   *
+   * @param n      number of rows and cols.
+   * @param square
+   */
+  public MagicSquare(int n, Integer[][] square) {
+    this.n = n;
+    this.square = square;
+  }
+
+  /**
+   * Getter for square
+   *
+   * @return {@code this.square}
+   */
+  public Integer[][] getSquare() {
+
+    return square;
+  }
+
+  /**
+   * Setter for square.
+   *
+   * @param square
+   */
+  public void setSquare(Integer[][] square) {
+
+    this.square = square;
+  }
+
+  /**
+   * Check if {@code square} is magic square or not. (static helper method)
+   *
+   * @param square 2-Dimensional array
+   * @return {@code true} if {@code square} is magic, otherwise {@code false}
+   * <p>
+   * Sample Input { {16, 3, 2, 13}, {5, 10, 11, 8}, {9, 6, 7, 12}, {4, 15, 14, 1} }
+   * <p>
+   * Sample Output true
+   * <p>
+   * Explanation: The sums of each row and col are equal.
+   */
+  public static boolean isMagicSquare(Integer[][] square) {
+    // TODO: You need to implement this method.
+//        int[][] k;
+//        int sum = 0;
+//         for (int i = 0; i < square.length; i++){
+//            for (int j = 0; j < square.length; j++){
+//                sum = k[i][j] + sum;
+//            }
+//         }
+
+    int sum = 0;
+    for (int i = 0; i < square.length; i++) {
+      for (int j = 0; j < square.length; j++) {
+        sum = sum + square[i][j];
+
+      }
+    }
+
+    return false;
+  }
+
+  /**
+   * Check if {@code this.square} is magic square or not.
+   *
+   * @return {@code true} if {@code square} is magic, otherwise {@code false}
+   */
+  public boolean isMagicSquare() {
+
+    return isMagicSquare(this.square);
+  }
+}
