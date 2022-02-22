03d6acefbe5759aec1c7e8f32a1a4545233cfd3a
YIshihara11201 <yusukeishihara87@gmail.com>
2022-02-22 13:23:46 -0800

    complete assignment2

--- src/main/java/ca/ciccc/Assignment2.java
+++ src/main/java/ca/ciccc/Assignment2.java
@@ -1,5 +1,8 @@
 package ca.ciccc;
 
+import java.util.Arrays;
+import java.util.Enumeration;
+
 /**
  * This is the starter file for the Assignment 2
  *
@@ -7,92 +10,141 @@ package ca.ciccc;
  * @version 1.0
  */
 public class Assignment2 {
-    /**
-     * Write nested for-loops to produce the following output from the given input n.
-     * @param n number to create a pattern.
-     * @return pattern
-     *
-     * Sample Input / Output
-     * numberLoops(3);
-     *
-     * Sample Output
-     * ..1
-     * .22
-     * 333
-     *
-     */
-    public String numberLoops(int n) {
-        // Todo: your code goes here
 
-        return null;
+  /**
+   * Write nested for-loops to produce the following output from the given input n.
+   *
+   * @param n number to create a pattern.
+   * @return pattern
+   * <p>
+   * Sample Input / Output numberLoops(3);
+   * <p>
+   * Sample Output ..1 .22 333
+   */
+  public static String numberLoops(int n) {
+    // Todo: your code goes here
+    StringBuilder result = new StringBuilder("");
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= n; j++) {
+        if (j <= n - i) {
+          result.append(".");
+        } else {
+          result.append(i);
+        }
+      }
+      if (i != n) {
+        result.append("\n");
+      }
     }
 
-    /**
-     * Given an array of integers, return true if the number of 1s is greater than the number of 7s
-     * @param nums array of ints
-     * @return true if the number of 1s is greater than the number of 7s
-     */
-    public boolean countNumbers(int[] nums) {
-        // Todo: your code goes here
+    return result.toString();
+  }
 
-        return false;
+  /**
+   * Given an array of integers, return true if the number of 1s is greater than the number of 7s
+   *
+   * @param nums array of ints
+   * @return true if the number of 1s is greater than the number of 7s
+   */
+  public boolean countNumbers(int[] nums) {
+    // Todo: your code goes here
+    int ones = 0;
+    int sevens = 0;
+
+    for (int num : nums) {
+      if (num == 1) {
+        ones++;
+      } else if (num == 7) {
+        sevens++;
+      }
+
+      if (ones > (int) Math.ceil(nums.length / 2.0)) {
+        break;
+      } else if (sevens > (int) Math.ceil(nums.length / 2.0)) {
+        break;
+      }
     }
 
-    /**
-     * Return the sum of numbers in the array, returning 0 for an empty array. Except the number 13
-     * is very unlucky, so it does not count the number that comes immediately after a 13, also 13 itself
-     * does not count.
-     * @param nums array of numbers
-     * @return the sum except for 13
-     *
-     * Sample Input / Output
-     * sumExcept13({1, 2, 2, 1}) -> 6
-     * sumExcept13({1, 2, 2, 1, 13} -> 6
-     * sumExcept13({1, 2, 13, 2, 1, 13} -> 4
-     */
-    public int sumExcept13(int[] nums) {
-        // Todo: your code goes here
+    return ones > sevens;
+  }
 
-        return 0;
+  /**
+   * Return the sum of numbers in the array, returning 0 for an empty array. Except the number 13 is
+   * very unlucky, so it does not count the number that comes immediately after a 13, also 13 itself
+   * does not count.
+   *
+   * @param nums array of numbers
+   * @return the sum except for 13
+   * <p>
+   * Sample Input / Output sumExcept13({1, 2, 2, 1}) -> 6 sumExcept13({1, 2, 2, 1, 13} -> 6
+   * sumExcept13({1, 2, 13, 2, 1, 13} -> 4
+   */
+  public int sumExcept13(int[] nums) {
+    // Todo: your code goes here
+    int result = 0;
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] == 13) {
+        i += 1;
+        continue;
+      }
+      result += nums[i];
     }
+    return result;
+  }
 
-    /**
-     * Return an array that is "left shifted" by one -- so {6, 2, 5, 3} return {2, 5, 3, 6}.
-     * You may modify and return the given array, or return a new array.
-     * @param nums array of ints
-     * @return shifted array
-     *
-     * Sample Input / Output
-     * shiftArray({6, 2, 5, 3}) -> {2, 4, 3, 6}
-     * shiftArray({2, 3}) -> {3, 2}
-     * shiftArray({7}) -> {7}
-     */
-    public int[] shiftArray(int[] nums) {
-        // Todo: your code goes here
-
-        return null;
+  /**
+   * Return an array that is "left shifted" by one -- so {6, 2, 5, 3} return {2, 5, 3, 6}. You may
+   * modify and return the given array, or return a new array.
+   *
+   * @param nums array of ints
+   * @return shifted array
+   * <p>
+   * Sample Input / Output shiftArray({6, 2, 5, 3}) -> {2, 4, 3, 6} shiftArray({2, 3}) -> {3, 2}
+   * shiftArray({7}) -> {7}
+   */
+  public int[] shiftArray(int[] nums) {
+    // Todo: your code goes here
+    if (nums.length == 0) {
+      return nums;
     }
-
-    /**
-     * Return true if the array contains three increasing adjacent numbers, otherwise false.
-     * @param nums array of ints.
-     * @return true if the array contains three increasing adjacent numbers, otherwise false.
-     */
-    public boolean tripleIncreasingOrder(int[] nums) {
-        // Todo: your code goes here
-
-        return false;
+    int temp = nums[0];
+    for (int i = 0; i < nums.length - 1; i++) {
+      nums[i] = nums[i + 1];
+      nums[i + 1] = temp;
     }
+    nums[nums.length - 1] = temp;
 
-    /**
-     * Return true if the array contains the same number of odds and even numbers, otherwise false.
-     * @param nums array of ints.
-     * @return true if the array contains the same number of odds and even numbers, otherwise false.
-     */
-    public boolean evenOrOdd(int[] nums){
-        // Todo: your code goes here
+    return nums;
+  }
 
-        return false;
+  /**
+   * Return true if the array contains three increasing adjacent numbers, otherwise false.
+   *
+   * @param nums array of ints.
+   * @return true if the array contains three increasing adjacent numbers, otherwise false.
+   */
+  public boolean tripleIncreasingOrder(int[] nums) {
+    // Todo: your code goes here
+    for (int i = 0; i < nums.length - 2; i++) {
+      if (nums[i] == nums[i + 1] - 1 && nums[i + 1] == nums[i + 2] - 1) {
+        return true;
+      }
     }
+    return false;
+  }
+
+  /**
+   * Return true if the array contains the same number of odds and even numbers, otherwise false.
+   *
+   * @param nums array of ints.
+   * @return true if the array contains the same number of odds and even numbers, otherwise false.
+   */
+  public boolean evenOrOdd(int[] nums) {
+    // Todo: your code goes here
+    int odds = Arrays.stream(nums).filter(num -> num % 2 == 1).toArray().length;
+    int evens = Arrays.stream(nums).filter(num -> num % 2 == 0).toArray().length;
+
+    return odds == evens;
+  }
 }
 
--- src/main/java/ca/ciccc/MagicSquare.java
+++ src/main/java/ca/ciccc/MagicSquare.java
@@ -1,85 +1,111 @@
 package ca.ciccc;
 
+import java.util.Arrays;
+
 /**
- *
- * Magic Squares are square arrays of numbers that have the interesting property that
- * the numbers in each column, and in each row, all add up to the same total.
- *
+ * Magic Squares are square arrays of numbers that have the interesting property that the numbers in
+ * each column, and in each row, all add up to the same total.
+ * <p>
  * Given an n x n square of numbers, determine if it is magic square.
- *
- * Reference: {@link java.lang.Integer}
- * Integer is a wrapper class for {@code int} primitive type.
+ * <p>
+ * Reference: {@link java.lang.Integer} Integer is a wrapper class for {@code int} primitive type.
  */
 public class MagicSquare {
-    /* n rows and n cols square */
-    private int n;
-    /* 2-Dimensional array to represent the square */
-    private Integer[][] square;
 
-    /**
-     * Constructor that takes n as the number of rows and cols.
-     * @param n number of rows and cols.
-     */
-    public MagicSquare(int n) {
-        this.n = n;
-        this.square = new Integer[n][n];
-    }
+  /* n rows and n cols square */
+  private int n;
+  /* 2-Dimensional array to represent the square */
+  private Integer[][] square;
 
-    /**
-     * Constructor that takes n as the number of rows and cols.
-     * Initializes square 2-Dimensional array.
-     * @param n number of rows and cols.
-     * @param square
-     */
-    public MagicSquare(int n, Integer[][] square) {
-        this.n = n;
-        this.square = square;
-    }
+  /**
+   * Constructor that takes n as the number of rows and cols.
+   *
+   * @param n number of rows and cols.
+   */
+  public MagicSquare(int n) {
+    this.n = n;
+    this.square = new Integer[n][n];
+  }
 
-    /**
-     * Getter for square
-     * @return {@code this.square}
-     */
-    public Integer[][] getSquare() {
-        return square;
-    }
+  /**
+   * Constructor that takes n as the number of rows and cols. Initializes square 2-Dimensional
+   * array.
+   *
+   * @param n      number of rows and cols.
+   * @param square
+   */
+  public MagicSquare(int n, Integer[][] square) {
+    this.n = n;
+    this.square = square;
+  }
 
-    /**
-     * Setter for square.
-     * @param square
-     */
-    public void setSquare(Integer[][] square) {
-        this.square = square;
-    }
+  /**
+   * Getter for square
+   *
+   * @return {@code this.square}
+   */
+  public Integer[][] getSquare() {
+    return square;
+  }
 
-    /**
-     * Check if {@code square} is magic square or not. (static helper method)
-     * @param square 2-Dimensional array
-     * @return {@code true} if {@code square} is magic, otherwise {@code false}
-     *
-     * Sample Input
-     * {
-     *   {16, 3, 2, 13},
-     *   {5, 10, 11, 8},
-     *   {9, 6, 7, 12},
-     *   {4, 15, 14, 1}
-     * }
-     *
-     * Sample Output
-     * true
-     *
-     * Explanation: The sums of each row and col are equal.
-     */
-    public static boolean isMagicSquare(Integer[][] square) {
-        // TODO: You need to implement this method.
+  /**
+   * Setter for square.
+   *
+   * @param square
+   */
+  public void setSquare(Integer[][] square) {
+    this.square = square;
+  }
+
+  /**
+   * Check if {@code square} is magic square or not. (static helper method)
+   *
+   * @param square 2-Dimensional array
+   * @return {@code true} if {@code square} is magic, otherwise {@code false}
+   * <p>
+   * Sample Input { {16, 3, 2, 13}, {5, 10, 11, 8}, {9, 6, 7, 12}, {4, 15, 14, 1} }
+   * <p>
+   * Sample Output true
+   * <p>
+   * Explanation: The sums of each row and col are equal.
+   */
+  public static boolean isMagicSquare(Integer[][] square) {
+    int squareLen = square.length;
+    int target = Arrays.stream(square[0]).reduce(0, (a, b) -> a + b);
+
+    // TODO: You need to implement this method.
+    for (int i = 1; i < squareLen - 1; i++) {
+      int curr = Arrays.stream(square[i]).reduce(0, (a, b) -> a + b);
+      int next = Arrays.stream(square[i + 1]).reduce(0, (a, b) -> a + b);
+      if (curr != target || curr != next) {
         return false;
+      }
     }
 
-    /**
-     * Check if {@code this.square} is magic square or not.
-     * @return {@code true} if {@code square} is magic, otherwise {@code false}
-     */
-    public boolean isMagicSquare() {
-        return isMagicSquare(this.square);
+    for (int i = 0; i < squareLen - 1; i++) {
+      int finalI = i;
+      Integer[] curr = Arrays.stream(square)
+          .reduce(new Integer[]{0}, (a, b) -> new Integer[]{a[0] + b[finalI]});
+      Integer[] next = Arrays.stream(square)
+          .reduce(new Integer[]{0}, (a, b) -> new Integer[]{a[0] + b[finalI]});
+      if (curr[0] != target || (int)curr[0] != (int)next[0]) {
+        return false;
+      }
     }
+    
+    return true;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(isMagicSquare(new Integer[][]{{1, 2, 3}, {3, 1, 2}, {2, 3, 1}}));
+  }
+
+  /**
+   * Check if {@code this.square} is magic square or not.
+   *
+   * @return {@code true} if {@code square} is magic, otherwise {@code false}
+   */
+  public boolean isMagicSquare() {
+    return isMagicSquare(this.square);
+  }
 }
